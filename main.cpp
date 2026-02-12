15656672c60603ecf3c43f164260075e8b275d45
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-11 20:42:32 -0800

    IGNORE: debugging

--- main.cpp
+++ main.cpp
@@ -2,36 +2,27 @@
 #include <iostream>
 using namespace std;
 
-// int swap(int* ptrOne, int* ptrTwo);
-int swap(int a, int b);
+int swap(int* ptrOne, int* ptrTwo);
 
 int main() {
     int x = 5, y = 10;
 
-    int* ptrX = &x; // stores address of x
-    int* ptrY = &y; // store address of y
+    // int* ptrX = &x; // stores address of x
+    // int* ptrY = &y; // store address of y
 
     cout << "x = " << x << "  | y = " << y << endl;
 
     cout << "Swapping...\n";
-    // int sum = swap(x, y);
+    int sum = swap(&x, &y);
     cout << "x = " << x << " | y = " << y << endl;
     // cout << "sum = " << sum << endl;
     return 0;
 }
 
-int swap(int a, int b) {
+int swap(int* a, int* b) {
     int temp = *a;
     a = &b;
     b = &temp;
 
-    return *a + *b; //a + b;
+    // return *a + *b; //a + b;
 }
-
-// int swap(int* a, int* b) {
-//     int temp = *a;
-//     a = &b;
-//     b = &temp;
-//
-//     return *a + *b; //a + b;
-// }
